ad4fa26e2cc96f3f6e39ef72077e088300da9a8f
Dimazzz <dimaz270307@gmail.com>
2024-02-04 01:38:24 +0700

    pengumpulan seeder

---
+++ database/seeders/SiswaSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\str;
+
+class SiswaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        $getIdKelas = DB::table("Kelases")->InsertGetId([
+            'id_kelas' => rand(1,25),
+            'nama_kelas' => str::random(10),
+            'kompetensi_keahlian' => str::random(50),
+        ]);
+
+        $getIdSpp = DB::table("spps")->InsertGetId([
+            'id_spp' => rand(1, 10),
+            'tahun' => random_int(2000, 2024),
+            'nominal' => random_int(1, 20),
+        ]);
+
+        DB::table("siswas")->insert([
+            'nisn' => rand(1, 50),
+            'nis' => rand(1, 10),
+            'nama' => "Dimas",
+            'id_kelas' => $getIdKelas,
+            'alamat' => "Bekasi",
+            'no_telp' => "081282723922",
+            'id_spp' => $getIdSpp,
+        ]);
+    }
+}
